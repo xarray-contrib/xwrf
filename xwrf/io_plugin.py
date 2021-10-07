317a8782172a1895c082e71a484f463a5adf7aae
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-10-07 13:51:08 -0600

    Simplify internals by using netcdf backend

--- xwrf/io_plugin.py
+++ xwrf/io_plugin.py
@@ -1,43 +1,19 @@
+import itertools
 import os
 import pathlib
 import re
-from collections import OrderedDict
+import warnings
 
-import numpy as np
+import pandas as pd
 import xarray as xr
 
-_COORD_PAIR_MAP = {
-    'XLAT': ('XLAT', 'XLONG'),
-    'XLONG': ('XLAT', 'XLONG'),
-    'XLAT_M': ('XLAT_M', 'XLONG_M'),
-    'XLONG_M': ('XLAT_M', 'XLONG_M'),
-    'XLAT_U': ('XLAT_U', 'XLONG_U'),
-    'XLONG_U': ('XLAT_U', 'XLONG_U'),
-    'XLAT_V': ('XLAT_V', 'XLONG_V'),
-    'XLONG_V': ('XLAT_V', 'XLONG_V'),
-    'CLAT': ('CLAT', 'CLONG'),
-    'CLONG': ('CLAT', 'CLONG'),
-}
+_LAT_COORDS = ('XLAT', 'XLAT_M', 'XLAT_U', 'XLAT_V', 'CLAT', 'XLAT_C')
 
+_LON_COORDS = ('XLONG', 'XLONG_M', 'XLONG_U', 'XLONG_V', 'CLONG', 'XLONG_C')
 
-_COORD_VARS = (
-    'XLAT',
-    'XLONG',
-    'XLAT_M',
-    'XLONG_M',
-    'XLAT_U',
-    'XLONG_U',
-    'XLAT_V',
-    'XLONG_V',
-    'CLAT',
-    'CLONG',
-)
+_TIME_COORD_VARS = ('XTIME', 'Times')
 
-_LAT_COORDS = ('XLAT', 'XLAT_M', 'XLAT_U', 'XLAT_V', 'CLAT')
-
-_LON_COORDS = ('XLONG', 'XLONG_M', 'XLONG_U', 'XLONG_V', 'CLONG')
-
-_TIME_COORD_VARS = ('XTIME',)
+_ALL_COORDS = set(itertools.chain(*[_LAT_COORDS, _LON_COORDS, _TIME_COORD_VARS]))
 
 
 def is_remote_uri(path: str) -> bool:
@@ -58,124 +34,29 @@ def _normalize_path(path):
     return path
 
 
-def _get_coord_names(ds, varname):
-    """Returns a tuple of coordinate names for a given variable."""
-
-    lat_coord = None
-    lon_coord = None
-    time_coord = None
-
-    # WRF variables will have a coordinates attribute.  MET_EM files have
-    # a stagger attribute which indicates the coordinate variable.
-    try:
-        # WRF files
-        coord_attr = getattr(ds[varname], 'coordinates')
-    except AttributeError:
-        if varname in _COORD_VARS:
-            lat_coord, lon_coord = _COORD_PAIR_MAP[varname]
-            if 'XTIME' in ds.variables:
-                time_coord = 'XTIME'
-
-        elif varname in _TIME_COORD_VARS:
-            lat_coord = lon_coord = time_coord = None
-
-        else:
+def clean(dataset):
+    """
+    Clean up the dataset.
+    """
+    coords = set(dataset.variables).intersection(_ALL_COORDS)
+    dataset = dataset.set_coords(coords)
+    for coord in dataset.coords:
+        if coord in _TIME_COORD_VARS:
             try:
-                # met_em files or old WRF files
-                stag_attr = getattr(ds[varname], 'stagger')
-            except AttributeError:
-                lat_coord = lon_coord = None
+                dataset[coord].data = pd.to_datetime(
+                    list(map(lambda x: x.decode('utf-8'), dataset[coord].data.tolist())),
+                    format='%Y-%m-%d_%H:%M:%S',
+                )
+            except:
+                warnings.warn(f'Failed to parse time coordinate: {coord}', stacklevel=2)
 
-                # Let's just check for xlat and xlong in this case
-                if 'XLAT' in ds.variables:
-                    lat_coord, lon_coord = 'XLAT', 'XLONG'
+            finally:
+                break
 
-            else:
-                # For met_em files, use the stagger name to get the lat/lon var
-                lat_coord = f'XLAT_{stag_attr}'
-                lon_coord = f'XLONG_{stag_attr}'
-
-                # If this coord name is missing, it might be an old WRF file
-                if lat_coord not in ds.variables:
-                    lat_coord = lon_coord = None
-                    if 'XLAT' in ds.variables:
-                        lat_coord, lon_coord = 'XLAT', 'XLONG'
-
-    else:
-        if isinstance(coord_attr, str):
-            coord_names = coord_attr.split()
-        else:
-            coord_names = coord_attr.decode().split()
-
-        for name in coord_names:
-            if name in _LAT_COORDS:
-                lat_coord = name
-                continue
-            if name in _LON_COORDS:
-                lon_coord = name
-                continue
-            if name in _TIME_COORD_VARS:
-                time_coord = name
-                continue
-
-        if time_coord is not None:
-            # Make sure the time variable wasn't removed
-            try:
-                ds.variables[time_coord]
-            except KeyError:
-                time_coord = None
-    return lat_coord, lon_coord, time_coord
-
-
-def is_time_coord_variable(variable):
-    """Returns True if the variable is a time coordinate variable."""
-    return variable.name in _TIME_COORD_VARS
+    return dataset.squeeze()
 
 
 class WRFBackendEntrypoint(xr.backends.BackendEntrypoint):
-    def build_data_array(self, dataset, varname, multitime=False, timeidx=0):
-        variable = dataset[varname]
-        time_idx_or_slice = timeidx if not multitime else slice(None)
-        if len(variable.shape) > 1:
-            data = variable[time_idx_or_slice, :]
-        else:
-            data = variable[time_idx_or_slice]
-
-        # Want to preserve the time dimension
-        if not multitime:
-            data = data[np.newaxis, :] if len(variable.shape) > 1 else data[np.newaxis]
-        attrs = OrderedDict()
-        for key in variable.ncattrs():
-            attrs[key] = variable.getncattr(key)
-
-        dims = variable.dimensions[-data.ndim :]
-
-        lat_coord = lon_coord = time_coord = None
-        try:
-            if dims[-2] == 'south_north' and dims[-1] == 'west_east':
-                lat_coord, lon_coord, time_coord = _get_coord_names(dataset, varname)
-        except IndexError:
-            pass
-
-        coords = OrderedDict()
-
-        # Handle lat/lon coordinates and projection information if available
-        if lon_coord is not None and lat_coord is not None:
-            lon_variable = dataset.variables[lon_coord]
-            lat_variable = dataset.variables[lat_coord]
-            lon_coord_dims = lon_variable.dimensions
-            lat_coord_dims = lat_variable.dimensions
-
-            if time_coord is not None:
-                coords[time_coord] = dataset.variables[time_coord][:]
-
-            coords[lon_coord] = lon_coord_dims[1:], lon_variable[:][0, :]
-            coords[lat_coord] = lat_coord_dims[1:], lat_variable[:][0, :]
-
-        result = xr.DataArray(variable, attrs=attrs, dims=dims)
-        result = result.assign_coords(coords)
-        return result
-
     def open_dataset(
         self,
         filename_or_obj,
@@ -207,7 +88,19 @@ class WRFBackendEntrypoint(xr.backends.BackendEntrypoint):
             lock=lock,
             autoclose=autoclose,
         )
-        arrays = [self.build_data_array(store.ds, varname) for varname in store.ds.variables.keys()]
-        filtered_arrays = list(filter(lambda x: x.name not in _COORD_VARS, arrays))
 
-        return xr.merge(filtered_arrays).squeeze()
+        store_entrypoint = xr.backends.store.StoreBackendEntrypoint()
+
+        with xr.core.utils.close_on_error(store):
+            dataset = store_entrypoint.open_dataset(
+                store,
+                mask_and_scale=mask_and_scale,
+                decode_times=decode_times,
+                concat_characters=concat_characters,
+                decode_coords=decode_coords,
+                drop_variables=drop_variables,
+                use_cftime=use_cftime,
+                decode_timedelta=decode_timedelta,
+            )
+
+        return clean(dataset)
