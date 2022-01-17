8a618ae5ac01076ee7fb4b20884f5e3681d8d7ad
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2022-01-17 15:30:09 +0100

    Forgot untracked files

---
+++ tests/test_units_fixed.py
@@ -0,0 +1,22 @@
+import os
+import pathlib
+
+import numpy as np
+import pytest
+import xwrf
+import xarray as xr
+from netCDF4 import Dataset
+
+here = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+sample_file = here / 'sample-data' / 'wrfout_d03_2018-10-18_00:00:00_only_attrs.nc'
+
+
+@pytest.fixture(scope='session', params=[sample_file])
+def dataset(request):
+    return xr.open_dataset(request.param, engine=xwrf.WRFBackendEntrypoint)
+
+
+@pytest.mark.parametrize('var_name', ('THIS_IS_AN_IDEAL_RUN', 'SAVE_TOPO_FROM_REAL'))
+def test_unit_is_removed(dataset, var_name):
+    assert 'units' not in dataset[var_name].attrs
