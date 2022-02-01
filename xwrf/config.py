016e6807f73744279d274d4503f435a46b25ec80
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2022-02-01 17:06:18 +0100

    Fixed defaults error

--- xwrf/config.py
+++ xwrf/config.py
@@ -8,5 +8,5 @@ fn = os.path.join(os.path.dirname(__file__), 'config.yaml')
 with open(fn) as f:
     defaults = yaml.safe_load(f)
 
-config = Config('xwrf', defaults=list(defaults))
+config = Config('xwrf', defaults=[defaults])
 config.ensure_file(fn)
