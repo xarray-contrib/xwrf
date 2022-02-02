5d6f5d4b628477654c4c2eb5ce674c584f35a711
Julia Kent <jkent@ucar.edu>
2022-02-02 14:29:17 -0700

    update __init__

--- xwrf/__init__.py
+++ xwrf/__init__.py
@@ -5,6 +5,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
 from .io_plugin import WRFBackendEntrypoint
+from .tutorial import open_dataset
 
 try:
     __version__ = get_distribution(__name__).version
