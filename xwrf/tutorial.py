06fd5e0d9b3ca4d7ea987e1b62124e264b6c27f4
Julia Kent <jkent@ucar.edu>
2022-02-02 14:26:16 -0700

    add tutorial.py module

---
+++ xwrf/tutorial.py
@@ -0,0 +1,86 @@
+"""
+Useful for:
+* users learning xwrf
+* building tutorials in the documentation.
+"""
+import os
+import pathlib
+import xarray as xr
+
+_default_cache_dir_name = 'xwrf_tutorial_data'
+base_url = 'https://github.com/ncar-xdev/xwrf-data'
+version = 'main'
+
+
+def _construct_cache_dir(path):
+    import pooch
+
+    if isinstance(path, os.PathLike):
+        path = os.fspath(path)
+    elif path is None:
+        path = pooch.os_cache(_default_cache_dir_name)
+
+    return path
+
+
+sample_datasetss = {
+    'lambert_conformal': '',
+    'polar_stereographic': '',
+    'mercator': '',
+    'lat_lon_global': '',
+}
+
+
+
+# idea borrowed from Seaborn and Xarray
+def open_dataset(
+    name,
+    cache=True,
+    cache_dir=None,
+    *,
+    engine="xwrf",
+    **kws,
+):
+    """
+    Open a dataset from the online repository (requires internet).
+    If a local copy is found then always use that to avoid network traffic.
+    Available datasets:
+    * ``""``:
+    Parameters
+    """
+    try:
+        import pooch
+    except ImportError as e:
+        raise ImportError(
+            'tutorial.open_dataset depends on pooch to download and manage datasets.'
+            ' To proceed please install pooch.'
+        ) from e
+
+    logger = pooch.get_logger()
+    logger.setLevel('WARNING')
+
+    cache_dir = _construct_cache_dir(cache_dir)
+    if name in sample_datasets:
+        url = sample_datasets[name]
+    else:
+        path = pathlib.Path(name)
+
+        url = f'{base_url}/raw/{version}/{path.name}'
+
+    # retrieve the file
+    filepath = pooch.retrieve(url=url, known_hash=None, path=cache_dir)
+    ds = xr.open_dataset(filepath, engine="xwrf", **kws)
+    if not cache:
+        ds = ds.load()
+        pathlib.Path(filepath).unlink()
+
+    return ds
+
+
+def load_dataset(*args, **kwargs):
+    """
+    Open, load into memory, and close a dataset from the online repository
+    (requires internet)
+    """
+    with open_dataset(*args, **kwargs) as ds:
+        return ds.load()
