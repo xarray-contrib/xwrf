c6bbbe56d88f73f08712330017bf5f2efab67535
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-02-09 18:25:11 -0700

    Add test_accessors

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import importlib
+import typing
+
+import packaging
+import pytest
+
+
+def importorskip(module_name, minversion: str = None) -> tuple[bool, typing.Callable]:
+    try:
+        module = importlib.import_module(module_name)
+        has = True
+
+        if minversion and packaging.version.parse(module.__version__) < packaging.version.parse(
+            minversion
+        ):
+            raise ValueError(
+                f'{module_name} version {module.__version__} is less than {minversion}'
+            )
+    except (ImportError, ValueError):
+        has = False
+
+    return pytest.mark.skipif(
+        not has, reason=f'requires {module_name} with minimum version={minversion}'
+    )
