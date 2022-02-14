8dd7349919fc7c8969bc8c2df2452c11e0de2936
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2022-02-14 16:34:10 +0100

    Forgot to add test

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -13,6 +13,11 @@ def dummy_attrs_only_dataset(request):
     return xwrf.tutorial.open_dataset(request.param)
 
 
+@pytest.fixture(scope='session', params=['met_em.d01.2005-08-28_12:00:00.nc'])
+def met_em_dataset(request):
+    return xwrf.tutorial.open_dataset(request.param)
+
+
 @pytest.mark.parametrize('variable', ('Q2', 'PSFC'))
 def test_cf_attrs_added(dummy_dataset, variable):
     dataset = xwrf.postprocess._modify_attrs_to_cf(dummy_dataset)
@@ -23,5 +28,11 @@ def test_cf_attrs_added(dummy_dataset, variable):
 
 @pytest.mark.parametrize('variable', ('THIS_IS_AN_IDEAL_RUN', 'SAVE_TOPO_FROM_REAL'))
 def test_remove_invalid_units(dummy_attrs_only_dataset, variable):
-    dataset = xwrf.postprocess._remove_invalid_units(dummy_attrs_only_dataset)
+    dataset = xwrf.postprocess._make_units_pint_friendly(dummy_attrs_only_dataset)
     assert 'units' not in dataset[variable].attrs
+
+
+@pytest.mark.parametrize('variable', ('OL4', 'GREENFRAC'))
+def test_met_em_parsing(met_em_dataset, variable):
+    dataset = xwrf.postprocess._make_units_pint_friendly(met_em_dataset)
+    assert dataset[variable].attrs['units'] == 'dimensionless'
