b361de59f14f6be8c0b8bdda1f257d0ce630e515
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-02-24 19:47:18 -0700

    Update tests

--- tests/test_metpy.py
+++ tests/test_metpy.py
@@ -1,10 +1,7 @@
+import metpy
 import pytest
 
 import xwrf
-
-from . import importorskip
-
-datasets_to_test = xwrf.tutorial.sample_datasets.keys()
 
 
 @pytest.fixture(scope='session')
@@ -12,13 +9,15 @@ def sample_dataset(request):
     return xwrf.tutorial.open_dataset(request.param)
 
 
-@importorskip('metpy')
-@pytest.mark.parametrize('sample_dataset', set(datasets_to_test) - {'tiny'}, indirect=True)
+@pytest.mark.parametrize('sample_dataset', ['met_em_sample'], indirect=True)
 def test_metpy_quantify(sample_dataset):
-    sample_dataset.xwrf.postprocess(decode_times=False).metpy.quantify()
+    ds = sample_dataset.xwrf.postprocess(decode_times=False).metpy.quantify()
+    assert str(ds.PRES.data.units) == 'pascal'
+    assert str(ds.MAPFAC_U.data.units) == 'dimensionless'
 
 
-@importorskip('metpy')
-@pytest.mark.parametrize('sample_dataset', datasets_to_test, indirect=True)
+@pytest.mark.parametrize('sample_dataset', ['met_em_sample'], indirect=True)
 def test_metpy_parse_cf(sample_dataset):
-    sample_dataset.metpy.parse_cf()
+    ds = sample_dataset.xwrf.postprocess().metpy.parse_cf()
+    assert 'metpy_crs' in ds.coords
+    assert isinstance(ds.metpy_crs.data.item(), metpy.plots.mapping.CFProjection)
