27f847edb8c4656a3d580bc0bc982238e51af64a
Kevin Paul <kpaul@ucar.edu>
2022-04-18 12:57:39 -0600

    Add custom JS to fix furo theme changes

---
+++ docs/source/_static/custom.js
@@ -0,0 +1,29 @@
+console.log('RUNNING CUSTOM JAVASCRIPT')
+
+function getBodyThemeName() {
+  const bodyTheme = document.body.dataset.theme
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  if (bodyTheme === 'auto') {
+    if (prefersDark) {
+      bodyTheme = 'dark'
+    } else {
+      bodyTheme = 'light'
+    }
+  }
+
+  return bodyTheme
+}
+
+document.documentElement.setAttribute('theme', getBodyThemeName())
+
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.type == 'attributes') {
+      var bodyTheme = getBodyThemeName()
+      console.log('document.body.dataset.theme changed to ' + bodyTheme)
+      document.documentElement.setAttribute('theme', bodyTheme)
+    }
+  })
+})
+observer.observe(document.body, { attributeFilter: ['data-theme'] })
