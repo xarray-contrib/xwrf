b2eb85ff544d332cb6adf06e9632c95920fff62f
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2022-09-20 00:16:19 +0200

    Added tests and CI deps

--- tests/test_udunits.py
+++ tests/test_udunits.py
@@ -10,6 +10,16 @@ def sample_dataset(request):
     return xwrf.tutorial.open_dataset(request.param)
 
 
+@pytest.fixture(scope='session')
+def sample_intake(request):
+    import intake
+
+    cat = intake.open_catalog(
+        'https://raw.githubusercontent.com/xarray-contrib/xwrf-data/main/catalogs/catalog.yml'
+    )
+    return cat[request.param].to_dask()
+
+
 @importorskip('cf_units')
 @pytest.mark.parametrize('sample_dataset', ['lambert_conformal'], indirect=True)
 @pytest.mark.parametrize(
@@ -48,4 +58,24 @@ def test_problematic_units_against_udunits_met_em(sample_dataset, variable, cf_d
     from cf_units import Unit
 
     ds = sample_dataset.xwrf.postprocess()
+    assert Unit(ds[variable].attrs.get('units', '')).definition == cf_definition_string
+
+
+@importorskip('dask')
+@importorskip('intake')
+@importorskip('cf_units')
+@pytest.mark.parametrize('sample_intake', ['xwrf-sample-ssp245'], indirect=True)
+@pytest.mark.parametrize(
+    'variable,cf_definition_string',
+    (
+        ('TKMG3D', 'm.kg.s-3.K-1'),
+        ('TKSATU3D', 'm.kg.s-3.K-1'),
+        ('TKDRY3D', 'm.kg.s-3.K-1'),
+        ('CSOL3D', 'm-1.kg.s-2.K-1'),
+    ),
+)
+def test_problematic_units_against_udunits_intake(sample_intake, variable, cf_definition_string):
+    from cf_units import Unit
+
+    ds = sample_intake.xwrf.postprocess()
     assert Unit(ds[variable].attrs.get('units', '')).definition == cf_definition_string
