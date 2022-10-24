6a8367c9d7ffb0eef038c13e13a1a4c6ec704c7b
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2022-10-24 22:48:30 +0200

    Amended unit harmonization

--- xwrf/config.py
+++ xwrf/config.py
@@ -8,5 +8,9 @@ fn = os.path.join(os.path.dirname(__file__), 'config.yaml')
 with open(fn) as f:
     defaults = yaml.safe_load(f)
 
+unit_harm_fn = os.path.join(os.path.dirname(__file__), 'unit_harmonization_map.yaml')
+with open(unit_harm_fn) as f:
+    defaults['unit_harmonization_map'] = yaml.safe_load(f)
+
 config = Config('xwrf', defaults=[defaults])
 config.ensure_file(fn, comment=False)
