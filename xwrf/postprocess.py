ba11b1ed5c070f2bb225aacad1f9a4ea83d698a5
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2023-04-04 13:34:01 +0200

    Fixed decoding of timestamp by using more flexible ISO8601 instead of format string

--- xwrf/postprocess.py
+++ xwrf/postprocess.py
@@ -21,9 +21,7 @@ def _decode_times(ds: xr.Dataset) -> xr.Dataset:
             ds.Times.data.astype('str'), errors='raise', format='%Y-%m-%d_%H:%M:%S'
         )
     except ValueError:
-        _time = pd.to_datetime(
-            ds.Times.data.astype('str'), errors='raise', format='%Y-%m-%dT%H:%M:%S.%f'
-        )
+        _time = pd.to_datetime(ds.Times.data.astype('str'), errors='raise', format='ISO8601')
     ds = ds.assign_coords({'Time': _time})
     ds.Time.attrs = {'long_name': 'Time', 'standard_name': 'time'}
     # make XTIME be consistent with its description
