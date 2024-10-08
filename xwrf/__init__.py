4283c5f960cb60d5c3d2859085c2944a59aae04a
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2024-10-08 11:42:07 +0200

    Replaced pkg_resources with importlib because of depreciation in 3.12

--- xwrf/__init__.py
+++ xwrf/__init__.py
@@ -2,7 +2,7 @@
 # flake8: noqa
 """ Top-level module. """
 
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 from . import postprocess, tutorial
 from .accessors import WRFDataArrayAccessor, WRFDatasetAccessor
@@ -10,6 +10,6 @@ from .config import config
 from .version_report import show_versions
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
     __version__ = 'unknown'  # pragma: no cover
